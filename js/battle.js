80d198d94f8768aedd5844c62187fc950e95da29
ericstretton <eric.stretton@gmail.com>
2022-03-27 22:03:45 -0600

    built project from ground up.

--- js/battle.js
+++ js/battle.js
@@ -1,60 +1,105 @@
-function battlePokemon(selection) {
-    let party = document.getElementById('party');
-    let partySlot = document.createElement(`div`);
-    let pokemonName = document.createElement(`h1`);
-    let pokemonImg = document.createElement('img');
-    pokemonImg.src="https://www.pngkit.com/png/full/65-654006_pichu-deviantart-pichu-pokemon.png"
-    party.append(partySlot);
-    party.style.cssText="width:25%;border:solid red 5px;margin:10px";
-    partySlot.append(pokemonName);
-    party.append(pokemonImg);
-    pokemonName.innerText=selection;
-}
+// let value = Cookies.get("pichuChosen");
+// console.log(value);
+// let pokemon = JSON.parse(value);
+
+let value2 = Cookies.get("mewChosen");
+console.log(value2);
+let pokemon2 = JSON.parse(value2);
+
+// let value3 = Cookies.get("charmanderChosen");
+// console.log(value3);
+// let pokemon3 = JSON.parse(value3);
 
 
-// function battlePokemonImage(selection){
-let cookieParty = JSON.parse(Cookies.get(`battleParty`));
-console.log(cookieParty);
 
 
-// let cookieImg = JSON.parse(Cookies.get(`mewImage`));
-// console.log(cookieImg);
-// }
-
-
-// Display pokemon that was gathered from index.html
-battlePokemon(cookieParty);
-// battlePokemon(pokemonImg);
-
-function Object(){
-    let partySlot = document.getElementById('party').querySelector('div').createElement('h2');
-    partySlot.append(healthPoints);
+function enterPichu(pokemon){
+    let newDiv = document.createElement('div');
+    newDiv.style.borderBottom = "3px solid white";
+    newDiv.classList.add('grid');
+    let pokemonSlot = document.getElementById("battler");
+    pokemonSlot.style.border = `3px solid white`;
     
-    let party = {
-            newAttr: {
-                name: 'Pichu',
-                healthPoints:100,
-                attackPoints:25,
-                attackOptions: ['Tackle', 'Leer', 'Thundershock', 'Electrocute'],
-            },
-            newAttr: {
-                name: 'Mew',
-                healthPoints:100,
-                attackPoints:30,
-                attackOptions: ['Psychic', 'Confusion', 'Teleport', 'Copycat']
-            } ,
-            newAttr: {
-                name: 'Charmander',
-                healthPoints:100,
-                attackPoints:35,
-                attackOptions: ['Ember','Flamethrower','Blaze','Fire Spin']
-            }
-        }
-        
-        let outputstring = "";
-for (const party of Object){
-    outputstring += `${newAttr.name} ${newAttr.healthPoints} ${newAttr.attackPoints} ${newAttr.attackOptions}` ;
-    party(Object);
+    let monImage = document.createElement('img');
+    monImage.setAttribute('src', pokemon.imgLink);
+    monImage.style.width = "200px";
+    newDiv.append(monImage);
+    
+    let pokemonName = document.createElement('h1');
+    pokemonName.innerText = pokemon.monName;
+    newDiv.append(pokemonName);
+
+    let pokemonStat = document.createElement('p');
+    pokemonStat.innerText = pokemon.healthPoints;
+    newDiv.append(pokemonStat);
+
+    document.getElementById("battler").append(newDiv);
 }
-console.log(party);
-    }
+
+
+function enterMew(pokemon2) {
+    let div2 = document.createElement('div');
+    div2.style.border = "3px solid pink";
+    div2.classList.add('grid');
+
+    let mewImage = document.createElement('img');
+    mewImage.setAttribute('src', pokemon2.imgLink);
+    mewImage.style.width = "200px";
+    div2.append(mewImage);
+
+    let pokemonName2 = document.createElement('h1');
+    pokemonName2.innerText = pokemon2.monName;
+    div2.append(pokemonName2);
+    
+    let pokemonStat2 = document.createElement('p');
+    pokemonStat2.innerText = pokemon2.healthPoints;
+    div2.append(pokemonStat2);
+    
+    document.getElementById("battler").append(div2);
+}
+
+function enterCharmander(pokemon3){
+    let newDiv = document.createElement('div');
+    newDiv.style.borderBottom = "3px solid white";
+    newDiv.classList.add('grid');
+    let pokemonSlot = document.getElementById("battler");
+    pokemonSlot.style.border = `3px solid white`;
+    
+    let monImage = document.createElement('img');
+    monImage.setAttribute('src', pokemon3.imgLink);
+    monImage.style.width = "200px";
+    newDiv.append(monImage);
+    
+    let pokemonName = document.createElement('h1');
+    pokemonName.innerText = pokemon3.monName;
+    newDiv.append(pokemonName);
+
+    let pokemonStat = document.createElement('p');
+    pokemonStat.innerText = pokemon3.healthPoints;
+    newDiv.append(pokemonStat);
+
+    document.getElementById("battler").append(newDiv);
+}
+
+
+// for (var i=0; i<pokemon.length; i++){
+//     enterPichu(pokemon[i]);
+// }
+// console.log(enterPichu(pokemon[i]));
+
+// enterPichu(pokemon);
+
+for (var i=0; i<pokemon2.length; i++){
+    enterMew(pokemon2[i]);
+}
+console.log(enterMew(pokemon2[i]));
+
+enterMew(pokemon2);
+
+// for (var i=0; i<pokemon3.length; i++){
+//     enterCharmander(pokemon3[i]);
+// }
+// console.log(enterCharmander(pokemon3[i]));
+
+// enterCharmander(pokemon3);
+
